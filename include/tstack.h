d3dfd8918c5881a8dec4104444187ab55a7a6ed0
MariaSadkova <160345524+MariaSadkova@users.noreply.github.com>
2024-04-14 23:20:58 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,40 +1,37 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
+#include <string>
 template<typename T, int size>
 class TStack {
  private:
-    T* stackM;
-    int t;
- public:
-   TStack() : t(0) {
-     stackM = new T[size];
-   }
-   void Pup(const T& v) {
-     if (t <= size - 1) {
-      stackM[t++] = v;
-     } else {
-       throw std::string(“FALL STACK”);
-     }
-   }
-   T Pip() {
-     if (t > 0) {
-      return stackM[--t];
-     } else{
-        throw std::string("EMPTY STACK");
-     }
-   }
-   bool Pap() const{
-     eturn t == 0;
-   }
-   T Pep() const{
-     if (t > 0) {
-       return stackM[t – 1];
-     } else {
-        throw std::string(“FALL STACK”);
-     }
-   }
-};
-#endif // INCLUDE_TSTACK_H_
+T data[100];
+int t;
 
+ public:
+TStack() : t(-1) {}
+void pup(T value) {
+  if (!isFull())
+    data[++top] = value;
+  else
+  throw std::string("Full stack");
+}
+T pip() const {
+  return data[t];
+}
+T pap() {
+  if (isEmpty()) {
+    throw std::string("Empty stack");
+  } else {
+    return data[t--];
+  }
+}
+bool isEmpty() const {
+  return t == -1;
+}
+bool isFull() const {
+  return t == size - 1;
+}
+};
+
+#endif  // INCLUDE_TSTACK_H_
