bcfbb73b4c053463284bfc4c1c659e17eb42c8be
MariaSadkova <160345524+MariaSadkova@users.noreply.github.com>
2024-04-15 01:18:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,76 +1,100 @@
 // Copyright 2021 NNTU-CS
+#include <math.h>
 #include <string>
 #include <map>
 #include "tstack.h"
-TStack<char, 100> stack1;
-TStack<int, 100> stack2;
-int perv(char k) {
-    if (k == '-') return 1;
-    if (k == '+') return 1;
-    if (k == '*') return 2;
-    if (k == '/') return 2;
+
+int ToInt(const std::string& str) {
+  int size = str.size();
+  int it = 0;
+  for (int i = 0; str[i]; i++) {
+    it += (pow(10, size - 1)) * (str[i]-'0');
+    size--;
+  }
+  return it;
+}
+int znaki(int a, int b, char chislo) {
+  switch (chislo) {
+  case '+':
+    return a + b;
+  case '-':
+    return a - b;
+  case '*':
+    return a * b;
+  case '/':
+    return a / b;
+  }
+  return 0;
+}
+int priora(char chislo) {
+  if (chislo == '(')
     return 0;
+  else if (chislo == ')')
+    return 1;
+  else if (chislo == '+' || chislo == '-')
+    return 2;
+  else if (chislo == '/' || chislo == '*')
+    return 3;
+  else
+    return -1;
 }
 std::string infx2pstfx(std::string inf) {
-    std::string post;
-    TStack<char, 100> stack;
-    for (char t : inf) {
-        if (isdigit(t)) {
-            post += t;
-            post += ' ';
-        } else if (t == '(') {
-            stack.push(t);
-        } else if (t == '+' || t == '-' || t == '*' || t == '/') {
-            while (!stack.isEmpty() && perv(stack.see()) >= perv(t)) {
-                post = post + stack.see() + ' ';
-                stack.pop();
-            }
-            stack.push(t);
-        } else if (t == ')') {
-            while (!stack.isEmpty() && stack.see() != '(') {
-                post = post + stack.see() + ' ';
-                stack.pop();
-            }
-            stack.pop();
+  bool flag = 0;
+  TStack<char, 100> stack;
+  std::string pref;
+  int prior = 0;
+  for (int i = 0; inf[i]; i++) {
+    if (inf[i] >= '0' && inf[i] <= '9') {
+      pref += inf[i];
+      if (inf[i+1] < '0' || inf[i+1] > '9') {
+        pref += ' ';
+      }
+      flag = 1;
+    } else if (inf[i] != '\0') {
+      prior = priora(inf[i]);
+      if (inf[i] == ')') {
+        while (stack.see() != '(') {
+          pref+= stack.rok();
+          pref += ' ';
         }
+        stack.rok();
+      } else if (prior == 0 || prior > priora(stack.see())
+        || stack.see() == -1) {
+        stack.map(inf[i]);
+      } else if (priora(stack.see()) >= prior) {
+          while (priora(stack.see()) >= prior) {
+              pref += stack.rok();
+          }
+          pref += ' ';
+          stack.map(inf[i]);
+      }
     }
-    while (!stack.isEmpty()) {
-        post = post + stack.see() + ' ';
-        stack.pop();
-    }
-    if (!post.empty()) {
-        post.pop_back();
-    }
-    return post;
+  }
+  while (stack.see() != -1) {
+    pref += stack.rok();
+    pref += ' ';
+  }
+  pref.pop_back();
+  return pref;
 }
 int eval(std::string pref) {
-    std::string strNumber = "";
-    for (char t : pref) {
-        if (t != ' ' && t != '+' && t != '-' && t != '*' && t != '/') {
-            strNumber += t;
-        }
-        if (t == ' ') {
-            if (!strNumber.empty()) {
-                int numer = std::stoi(strNumber);
-                stack2.push(numer);
-                strNumber = "";
-            }
-        }
-        if (t == '+' || t == '-' || t == '*' || t == '/') {
-            int vt = stack2.see();
-            stack2.pop();
-            int per = stack2.see();
-            stack2.pop();
-            if (t == '+') {
-                stack2.push((per + vt));
-            } else if (t == '-') {
-                stack2.push((per - vt));
-            } else if (t == '*') {
-                stack2.push((per * vt));
-            } else if (t == '/') {
-                stack2.push((per / vt));
-            }
-        }
+  std::string fake;
+  int a = 0, b = 0;
+  TStack<int, 100> stack;
+  for (int i = 0; pref[i]; i++) {
+    if (pref[i] >= '0' && pref[i] <= '9') {
+      while (pref[i] >= '0' && pref[i] <= '9') {
+        fake += pref[i++];
+      }
+      stack.map(ToInt(fake));
+      fake = "";
+    } else if (pref[i] == ' ') {
+      continue;
+    } else {
+      a = stack.rok();
+      b = stack.rok();
+      stack.map(znaki(b, a, pref[i]));
     }
-    return stack2.see();
+  }
+  return stack.rok();
 }
