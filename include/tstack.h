8688318eadfa018e0c71d017c0f43bed7a0ab2f5
MariaSadkova <160345524+MariaSadkova@users.noreply.github.com>
2024-04-15 00:05:15 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,36 +2,31 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 #include <string>
+
 template<typename T, int size>
 class TStack {
  private:
-T data[100];
-int m;
-
+    T data[100];
+    int m;
  public:
-TStack() : m(-1) {}
-void pup(T value) {
-  if (!isFull())
-    data[++m] = value;
-  else
-  throw std::string("Full stack");
-}
-T pip() const {
-  return data[m];
-}
-T pap() {
-  if (isEmpty()) {
-    throw std::string("Empty stack");
-  } else {
-    return data[m--];
-  }
-}
-bool isEmpty() const {
-  return m == -1;
-}
-bool isFull() const {
-  return m == size - 1;
-}
+    TStack(): m(-1) {}
+    void pushh(T vl) {
+      if (!isOverflow())
+        arr[++m] = vl;
+      else
+        throw std::string("Fall stack");
+    }
+    T popp() {
+      if (isEmptyy())
+        throw std::string("Empty stack");
+      else
+        return arr[m--];
+    }
+    bool isOverflow() const {
+      return m == size - 1;
+    }
+    bool isEmptyy() const {
+      return m == -1;
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
