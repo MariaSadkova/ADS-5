19f501c5f9ae4048a1fc8f0b57149586ae9db2cd
MariaSadkova <160345524+MariaSadkova@users.noreply.github.com>
2024-04-14 23:51:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,75 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+TStack<char, 100> stack1;
+TStack<int, 100> stack2;
+int perv(char k) {
+    if (k == '-') return 1;
+    if (k == '+') return 1;
+    if (k == '*') return 2;
+    if (k == '/') return 2;
+    return 0;
 }
-
+std::string infx2pstfx(std::string inf) {
+    std::string post;
+    TStack<char, 100> stack;
+    for (char t : inf) {
+        if (isdigit(t)) {
+            post += t;
+            post += ' ';
+        } else if (t == '(') {
+            stack.push(t);
+        } else if (t == '+' || t == '-' || t == '*' || t == '/') {
+            while (!stack.isEmpty() && perv(stack.see()) >= perv(c)) {
+                post = post + stack.see() + ' ';
+                stack.pop();
+            }
+            stack.push(t);
+        } else if (t == ')') {
+            while (!stack.isEmpty() && stack.see() != '(') {
+                post = post + stack.see() + ' ';
+                stack.pop();
+            }
+            stack.pop();
+        }
+    }
+    while (!stack.isEmpty()) {
+        post = post + stack.see() + ' ';
+        stack.pop();
+    }
+    if (!post.empty()) {
+        post.pop_back();
+    }
+    return post;
+}
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+    std::string strNumber = "";
+    for (char t : pref) {
+        if (t != ' ' && t != '+' && t != '-' && t != '*' && t != '/') {
+            strNumber += t;
+        }
+        if (t == ' ') {
+            if (!strNumber.empty()) {
+                int numer = std::stoi(strNumber);
+                stack2.push(numer);
+                strNumber = "";
+            }
+        }
+        if (t == '+' || t == '-' || t == '*' || t == '/') {
+            int vt = stack2.see();
+            stack2.pop();
+            int per = stack2.see();
+            stack2.pop();
+            if (t == '+') {
+                stack2.push((per + vt));
+            } else if (t == '-') {
+                stack2.push((per - vt));
+            } else if (t == '*') {
+                stack2.push((per * vt));
+            } else if (t == '/') {
+                stack2.push((per / vt));
+            }
+        }
+    }
+    return stack2.see();
 }
